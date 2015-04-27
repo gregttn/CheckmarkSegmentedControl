3cb7ee2fb899f2baf443f20d7ecf688e313f60d7
gregttn <gregttn@gmail.com>
2015-04-27 09:01:29 +0100

    init

---
+++ CheckmarkSegmentedControl/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CheckmarkSegmentedControl
+//
+//  Created by Grzegorz Tatarzyn on 27/04/2015.
+//  Copyright (c) 2015 gregttn. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
