836c6b352fba1e4bc59baa44d949c73d512f4005
gregttn <gregttn@gmail.com>
2015-05-03 21:35:30 +0100

    Place label at the bottom of the container for each element of the segmented control

---
+++ CheckmarkSegmentedControl/CheckmarkSegmentedControl.swift
@@ -0,0 +1,54 @@
+//
+//  CheckmarkSegmentedControl.swift
+//  CheckmarkSegmentedControl
+//
+//  Created by Grzegorz Tatarzyn on 27/04/2015.
+//  Copyright (c) 2015 gregttn. All rights reserved.
+//
+
+import UIKit
+
+class CheckmarkSegmentedControl: UIControl {
+    var titles: [String] = []
+    var titleFont: UIFont = UIFont.systemFontOfSize(12.0)
+    
+    required init(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+    }
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+    
+    }
+    
+    override func drawRect(rect: CGRect) {
+        let sectionSize: CGSize = CGSizeMake(rect.width / CGFloat(titles.count), rect.height)
+    
+        for index in (0..<titles.count) {
+            let sectionOffset = CGPoint(x: sectionSize.width * CGFloat(index), y: 0)
+
+            let label = createTitleLabel(sectionSize, continerOffset: sectionOffset, content: titles[index])
+            layer.addSublayer(label)
+        }
+    }
+    
+    private func createTitleLabel(containerSize: CGSize, continerOffset: CGPoint, content: String) -> CATextLayer {
+        let labelSize = sizeForLabel(content)
+        let labelFrame = CGRectMake(continerOffset.x, containerSize.height - labelSize.height, labelSize.width, labelSize.height)
+        
+        let label: CATextLayer = CATextLayer()
+        label.frame = labelFrame
+        label.font = titleFont
+        label.fontSize = titleFont.pointSize
+        label.string = content
+        
+        return label
+    }
+    
+    private func sizeForLabel(text: String) -> CGSize {
+        let textAttributes = [NSFontAttributeName : titleFont]
+        let string: NSString = text
+        
+        return string.sizeWithAttributes(textAttributes)
+    }
+}
--- CheckmarkSegmentedControlTests/CheckmarkSegmentedControlTests.swift
+++ CheckmarkSegmentedControlTests/CheckmarkSegmentedControlTests.swift
@@ -10,10 +10,15 @@ import UIKit
 import XCTest
 
 class CheckmarkSegmentedControlTests: XCTestCase {
+    var checkmark: CheckmarkSegmentedControl!
+    let titles: [String] = ["option 1", "option 2"]
     
     override func setUp() {
         super.setUp()
         // Put setup code here. This method is called before the invocation of each test method in the class.
+        
+        checkmark = CheckmarkSegmentedControl(frame: CGRectMake(0, 0, 50, 60))
+        checkmark.titles = titles
     }
     
     override func tearDown() {
@@ -21,16 +26,33 @@ class CheckmarkSegmentedControlTests: XCTestCase {
         super.tearDown()
     }
     
-    func testExample() {
-        // This is an example of a functional test case.
-        XCTAssert(true, "Pass")
-    }
-    
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measureBlock() {
-            // Put the code you want to measure the time of here.
+    func testShouldPlaceTitlesAtTheBottomOfTheConainer() {
+        checkmark.drawRect(checkmark.frame)
+        
+        for index in (0..<titles.count) {
+            let titleLayer: CATextLayer = checkmark.layer.sublayers[index] as! CATextLayer
+            let expectedSize = sizeForText(checkmark.titles[index], font: checkmark.titleFont)
+        
+            let expectedOrigin = CGPoint(x: CGFloat(index) * checkmark.frame.width / CGFloat(checkmark.titles.count), y:checkmark.frame.height - expectedSize.height)
+            XCTAssertEqualWithAccuracy(titleLayer.frame.origin.x, expectedOrigin.x, 0.1, "Incorrect x origin for: \(checkmark.titles[index])")
+            XCTAssertEqualWithAccuracy(titleLayer.frame.origin.y, expectedOrigin.y, 0.1, "Incorrect x origin for: \(checkmark.titles[index])")
         }
     }
     
+    func testTitleLablesShouldHaveCorrectTitels() {
+        checkmark.drawRect(checkmark.frame)
+        
+        for index in (0..<titles.count) {
+            let titleLayer: CATextLayer = checkmark.layer.sublayers[index] as! CATextLayer
+            
+            XCTAssertEqual(titleLayer.string as! String, checkmark.titles[index])
+        }
+    }
+
+    private func sizeForText(text: String, font: UIFont) -> CGSize {
+        let textAttributes = [NSFontAttributeName : font]
+        let string: NSString = text
+
+        return string.sizeWithAttributes(textAttributes)
+    }
 }
